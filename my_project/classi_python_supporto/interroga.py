90e5f16d59830c396c4afde8420c00f2ef94985e
SimoneBollini <simone.bollini@studio.unibo.it>
2025-02-22 17:30:00 +0100

    Add LangChain retriever documentation and update example query in interroga.py

--- my_project/classi_python_supporto/interroga.py
+++ my_project/classi_python_supporto/interroga.py
@@ -4,7 +4,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 # 1. Carica il modello di embedding nel formato corretto
 embedder = HuggingFaceEmbeddings(
     model_name="BAAI/bge-m3",
-    model_kwargs={'device': 'cpu'},  # Usa 'cuda' per GPU
+    model_kwargs={'device': 'cpu'},
     encode_kwargs={'normalize_embeddings': True}
 )
 
@@ -16,16 +16,17 @@ vector_store = FAISS.load_local(
 )
 
 # 3. Query di esempio
-query = ("Scrivimi una funzione per calcolare le statistiche di una squadra di basket")
-#"Cosa ritorna il metodo segnaleWow(LocalDate.of(2025, 1, 10)) che utilizza la funzione getMessaggioMagico() della libreria DateUtilCustom?"
+query = "Cosa ritorna il metodo segnaleWow(LocalDate.of(2025, 1, 10)) che utilizza la funzione getMessaggioMagico() della libreria DateUtilCustom?"
 #"Cosa ritorna il metodo segnaleWow(LocalDate.of(2025, 1, 10))?"
+#"Scrivimi una funzione per calcolare le statistiche di una squadra di basket")
+
 
 # 4. Cerca i chunk più simili
 docs = vector_store.similarity_search_with_score(
     query,
     k=5,
-    score_threshold=0.90,  # medio-bassa similarità inizialmente era 0.90
-    search_type="similarity",  # Più efficace per il codice usare mmr per diversità
+    score_threshold=0.90,  # medio-bassa similarità
+    search_type="similarity",  # Più efficace per il codice
     lambda_mult=0.5       # Bilancia diversità/rilevanza
 )
 
